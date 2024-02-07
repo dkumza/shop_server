926c721ef5e460713a06a0e0aca9c3cff44fbfd2
Darius <dariuskumza@gmail.com>
2024-02-07 12:25:27 +0200

    Update authToken middleware logging

--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -7,14 +7,17 @@ const { jwtSecret } = require('../config/config');
 module.exports = function authToken(req, res, next) {
   console.log('authToken in progress');
   try {
-    console.log('req.headers.authorization: ', req.headers.authorization);
+    console.log(
+      chalk.bgGreen.whiteBright('req.headers.authorization: '),
+      req.headers.authorization,
+    );
     const token = req.headers.authorization.split(' ')[1];
     if (!token) throw new Error('no token');
     const decoded = jwt.verify(token, jwtSecret);
     req.userID = decoded.sub;
     next();
   } catch (error) {
-    console.log('error ===', error);
+    console.log(chalk.bgRed.whiteBright('error: '), error);
     res.status(401).json('Unauthorized');
   }
 };
