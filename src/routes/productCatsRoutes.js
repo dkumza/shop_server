ef3b85175c15c3701e4ba03cc143d1adb6c1ead4
Darius <dariuskumza@gmail.com>
2024-02-25 17:28:19 +0200

    Remove authToken middleware from productCatsRouter.get('/categories') route

--- src/routes/productCatsRoutes.js
+++ src/routes/productCatsRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const productCatsRouter = express.Router();
 const productCatControllers = require('../controllers/productCatControllers');
-const authToken = require('../middleware/authToken');
 
 // ROUTES //
 // GET all categories
-productCatsRouter.get('/categories', authToken, productCatControllers.getCategories);
+productCatsRouter.get('/categories', productCatControllers.getCategories);
 // GET all subcategories
 productCatsRouter.get('/sub-categories', productCatControllers.getAllSubCategories);
 // GET single sub category, by category ID
